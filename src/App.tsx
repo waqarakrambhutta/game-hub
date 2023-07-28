b32ca1bf8ebaee8ca2d5413dd0c5b1d97f6cb5e7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-29 03:33:42 +0500

    Fetching games

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
-import './App.css'
-import NavBar from './components/NavBar';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
- return (<Grid templateAreas={{
-  base: `"nav" "main"`,
-  lg:`"nav nav" "aside main"`
- }}>
-  <GridItem area='nav'><NavBar/></GridItem>
-  <Show above='lg'> 
-  <GridItem area='aside'>Aside</GridItem>
-  </Show>
-  <GridItem area='main'>Main</GridItem>
- </Grid>
- );
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      
+      <GridItem area="main"><GameGrid/></GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL:'https://api.rawg.io/api',
+    params:{
+        key: '3f0f0f7dccbc43579896269002b7fd6c'
+    }
+})
