b3f48a59f07649b750b7f306ddba6f8156b12178
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-28 23:08:38 +0500

    creating a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
- return <Button colorScheme='blue'>Button</Button>
-
+ return (<Grid templateAreas={{
+  base: `"nav" "main"`,
+  lg:`"nav nav" "aside main"`
+ }}>
+  <GridItem area='nav' bg='blue'>Nav</GridItem>
+ {/* in this tag we write tags to show on large devices */}
+  <Show above='lg'> 
+  <GridItem area='aside' bg='orange'>Aside</GridItem>
+  </Show>
+  <GridItem area='main' bg='green'>Main</GridItem>
+ </Grid>
+ );
 }
 
 export default App
