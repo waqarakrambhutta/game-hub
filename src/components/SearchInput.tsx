e2fcf38f106161715bb6eb22b8a9a1ea7c4a4fd3
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-08-05 01:55:05 +0500

    NOTHING

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,21 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import React from 'react'
+import { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
 
 const SearchInput = () => {
+
+  const ref = useRef<HTMLInputElement>(null)
+
   return (
+    <form onSubmit={(event)=>{ 
+    event.preventDefault()
+    if (ref.current) console.log(ref.current.value);
+    }}>
     <InputGroup>
     <InputLeftElement children={<BsSearch/>}/>
     <Input borderRadius={20} placeholder='Search for game...'></Input>
     </InputGroup>
+    </form>
   )
 }
 
